e3f9481738ebf8c432601a056f3d7ad0e93c4650
Dean <lynchdean@gmail.com>
2020-08-06 14:43:23 +0100

    Remove unused import breaking build

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import Topbar from "./components/Topbar"
 import About from "./sections/About";
-import CardSlider from "./components/CardSlider";
 import Projects from "./sections/Projects";
 
 function App() {
