1ceb191e1c07c6ee736f5a4a28206d1e7df5524a
Dean <lynchdean@gmail.com>
2020-08-27 18:45:29 +0100

    Add WIP alert

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,17 @@ import React from 'react';
 import Topbar from "./components/Topbar"
 import About from "./sections/About";
 import Projects from "./sections/Projects";
+import Alert from "react-bootstrap/Alert";
 
 function App() {
     return (
         <div>
             <Topbar/>
+
+            <Alert variant="warning" className="text-center m-0">
+                This site is a <b>work-in-progress.</b> If you're interested, you can see more{' '}
+                <Alert.Link href="https://github.com/lynchdean/deanlynch-xyz">on the GitHub repo.</Alert.Link>
+            </Alert>
 
             <About/>
 
