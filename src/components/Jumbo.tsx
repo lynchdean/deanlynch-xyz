8e221fa9d3782122a8410b7c8c50d33854104df8
Dean <lynchdean@gmail.com>
2020-09-02 17:44:01 +0100

    Add buttons to Jumbo

--- src/components/Jumbo.tsx
+++ src/components/Jumbo.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 
+import Col from "react-bootstrap/Col";
 import "./css/jumbo.css"
 
 type Props = {
@@ -22,6 +23,14 @@ function Jumbo({heading, lines, cards}: Props) {
                         {lines.map((line, index) => {
                             return <p key={index} className="lead">{line}</p>
                         })}
+
+                        <div className="d-flex justify-content-center">
+                            <div className="btn-toolbar mx-auto pt-4">
+                                <Button href="https://www.linkedin.com/in/deanlynch97/" className="mr-3" variant="outline-light">LinkedIn</Button>
+                                <Button href="mailto:lynchdean@gmail.com?subject= Hello! (via deanlynch.xyz)" className="ml-3" variant="outline-light">Email</Button>
+                            </div>
+                        </div>
+
                     </Col>
                     <Col sm={4}>
                         <ul className={"p-0"} style={{listStyle: "none"}}>
