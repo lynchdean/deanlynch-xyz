9984a0c5392c1e617e3ad0cccf8a1ee0c2249eb8
Dean <lynchdean@gmail.com>
2021-07-28 22:27:34 +0100

    Update about

--- src/sections/data/about.js
+++ src/sections/data/about.js
@@ -1,10 +1,15 @@
 const data = {
     heading: "Hi, I'm Dean 👋",
     lines: [
-        "I graduated from Computer Applications & Software Engineering in Dublin City University.",
-        "I'm currently looking for a graduate or entry-level position in software engineering. I'm primarily interested in Python, Java, or JavaScript roles, but I'm open to anything else if it's a good opportunity for me to learn and gain experience.",
-        "I'm eager to start working so please get in touch with me via LinkedIn or email if you might have any suitable positions!"
+        "Currently working as a Software Test Engineer in the telecommunications industry, developing SMS firewall solutions.",
+        "Graduated from Dublin City University, with a BSc. in Computer Applications & Software Engineering",
+        "Seeking a Software Engineering role in an environment in which I can apply my skills and challenge myself, as well as learn and gain experience.",
+        "If you feel like you have a position available that you think I might be interested in, please get in touch with me via LinkedIn or Email."
     ],
+
+
+  
+
     cards: [
         {title: "DCU Graduate", image: require("../img/dcu-logo.png")},
         {title: "Dublin based", image: require("../img/dublin-crest.png")}
