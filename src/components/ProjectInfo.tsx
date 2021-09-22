d1dec0c3a7db39257606a8c55af0c3db9ecdedb6
Dean <lynchdean@gmail.com>
2021-09-22 20:06:53 +0100

    Mobile friendliness

--- src/components/ProjectInfo.tsx
+++ src/components/ProjectInfo.tsx
@@ -10,21 +10,21 @@ interface Props {
 function ProjectInfo({title, desc, tech, link}: Props) {
   return (
     <div className="row text-light">
-      <div className="col-sm-3 mb-auto">
+      <div className="col-md-3 mb-auto">
         <div className="row">
-          <div className="col-sm-2 mb-auto">
+          <div className="col-md-2 mb-auto">
             <i className="bi bi-file-earmark-code text-muted fs-3"/>
           </div>
-          <div className="col-sm-8">
+          <div className="col-md-8">
             <strong>{title}</strong>
           </div>
         </div>
       </div>
-      <div className="col-sm-6">{desc}</div>
-      <small className="col-sm-2 text-muted">{tech.join(", ")}</small>
+      <div className="col-md-6">{desc}</div>
+      <small className="col-md-2 text-muted">{tech.join(", ")}</small>
       <a
         href={link}
-        className="col-sm-1 btn btn-dark rounded-pill bg-dark text-secondary text-muted mb-auto p-2"
+        className="col-md-1 btn btn-dark rounded-pill bg-dark text-secondary text-muted mb-auto p-2"
         role="button"
       >
         <i className="bi bi-github"/> Link
